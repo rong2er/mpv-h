92cfac199c1aea459cc679cb2a7ea39c7f645a7a
rong2er <hexianfu520@gmail.com>
2026-08-31 15:11:30 +0800

    Update mpv.cmake

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -27,7 +27,8 @@ ExternalProject_Add(mpv
         curl
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG v0.41.0
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
