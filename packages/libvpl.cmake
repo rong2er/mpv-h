d7f30437c2e0b56df3c5327d85dd28a9007bff16
rong2er <hexianfu520@gmail.com>
2026-08-31 10:31:38 +0800

    Update libvpl.cmake

--- packages/libvpl.cmake
+++ packages/libvpl.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(libvpl
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_REMOTE_NAME origin
-    GIT_TAG main
+    GIT_TAG v2.10.0
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
